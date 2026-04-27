d0b15785694cd1028448c2989a0bae7b404b8309
Abdurrahman Adedokun Ademuyiwa <148814403+adigun001@users.noreply.github.com>
2026-04-27 23:53:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,19 +53,3 @@ filterButtons.forEach(button => {
         });
     });
 });
-
-
-
-document.querySelectorAll('.faq-question').forEach(button => {
-    button.addEventListener('click', () => {
-        const faqItem = button.parentElement;
-        
-        // Close other items (Optional)
-        document.querySelectorAll('.faq-item').forEach(item => {
-            if (item !== faqItem) item.classList.remove('active');
-        });
-
-        // Toggle current item
-        faqItem.classList.toggle('active');
-    });
-});
